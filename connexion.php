688118dcabac26d082d5cd34d215cf012e2ede1b
eliot-gsd <94835940+eliot-gsd@users.noreply.github.com>
2022-05-10 08:26:10 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -2,10 +2,6 @@
 <html>
 
     <head>
-        <meta charset="utf-8">
-        <link href="style.css" rel="stylesheet">
-        <link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
         <title>Page de connexion</title>
     </head>
 
@@ -15,32 +11,12 @@
     <nav>
     </nav>
 
-    <body class="text-center">
-        <main class="form-signin">
-            <form>
-                <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-                </div>
-
-                <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-                </div>
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-            </form>
+    <body>
+        <main>
         </main>
 
     </body>
+
 
 
 
