be00106bbd7fb4961be4cc007677a927f0327d19
eliot-gsd <94835940+eliot-gsd@users.noreply.github.com>
2022-05-10 08:26:38 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -12,9 +12,31 @@
     </nav>
 
     <body>
-        <main>
-        </main>
-
+        <fieldset>
+        <legend>Contact Form</legend>
+        <form name="frmContact" method="post" action="contact.php">
+        <p>
+        <label for="Name">Name </label>
+        <input type="text" name="txtName" id="txtName">
+        </p>
+        <p>
+        <label for="email">Email</label>
+        <input type="text" name="txtEmail" id="txtEmail">
+        </p>
+        <p>
+        <label for="phone">Phone</label>
+        <input type="text" name="txtPhone" id="txtPhone">
+        </p>
+        <p>
+        <label for="message">Message</label>
+        <textarea name="txtMessage" id="txtMessage"></textarea>
+        </p>
+        <p>&nbsp;</p>
+        <p>
+        <input type="submit" name="Submit" id="Submit" value="Submit">
+        </p>
+        </form>
+        </fieldset>
     </body>
 
 
