9b9a108696420eee2904285b49703c4c6915b057
adrien5322 <adrien.caudal53@gmail.com>
2022-05-18 17:02:23 +0200

    Update verif_form.js

--- verif_form.js
+++ verif_form.js
@@ -3,20 +3,24 @@ function validerForm(){
 	
 	var produit = document.getElementById("id_nom").value;	
 	
-	var prix = document.getElementById("id_pass");
+	var prix = document.getElementById("id_pass").value;
 	
-
-	
-	if(produit=="1"){
-		alert("Vous devez saisir un nom valide pour votre produit. ");
-		
-		
+	if (prix<0){
+		alert("Vous devez saisir un prix valide pour votre produit. ");
 	}
-	
 	else{
 		retour = true;
 	}
-	
-	
+	switch(prix)
+				{
+					case "0":	
+						alert("Vous devez saisir un nom valide pour votre produit. ");	
+					break;
+					case "2":	
+						alert("Carré");
+					break;
+					default:
+						retour = true;
+				}
 	return retour;
 }
